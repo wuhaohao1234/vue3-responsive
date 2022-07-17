f613a886fa53e4bd9ba7b471f86ffffc880b4494
wuhaohao <1611499758@qq.com>
2022-07-17 12:28:59 +0800

    feat: 添加reactivity方法，将dep添加到全局的targetMap

--- core/reactivity/index.js
+++ core/reactivity/index.js
@@ -33,13 +33,59 @@ const effectWatch = (effect) => {
   currentEffect = null
 }
 
-const dep = new Dep(10)
+// const dep = new Dep(10)
 
-let b
+// let b
 
-effectWatch(() => {
-  b = dep.value + 10
-  console.log(b);
+// effectWatch(() => {
+//   b = dep.value + 10
+//   console.log(b);
+// })
+
+// dep.value = 20
+
+const targetMap = new Map()
+
+function getMap(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Dep();
+    depsMap.set(key, dep);
+  }
+  return dep
+}
+
+const reactivity = (raw) => {
+  return new Proxy(raw, {
+    get(target, key) {
+      const dep = getMap(target, key);
+      dep.depend();
+      // return target[key]
+      return Reflect.get(target, key)
+    },
+    set(target, key, value) {
+        const dep = getMap(target, key);
+        const result = Reflect.set(target, key, value)
+        dep.notice()
+        return result
+    }
+  })
+}
+
+const user = reactivity({
+  age: 19
 })
 
-dep.value = 20
+let double;
+
+effectWatch(() => {
+  double = user.age + 10
+  console.log(double);
+})
+
+user.age = 20
