026165f17f1585a4702b770e50a394bab9f650fc
wuhaohao <1611499758@qq.com>
2022-07-17 11:10:09 +0800

    chore: 封装update方法

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-let a = 10;
-let b = a + 10;
+//  v1
+// let a = 10;
+// let b = a + 10;
 
-console.log(b);
+// console.log(b);
+
+// a = 20
+
+// b = a + 10
+
+// console.log(b);
+
+// v2
+
+let a = 10;
+let b
+
+function update() {
+  b = a + 10
+  console.log(b);
+}
+
+update()
 
 a = 20
 
-b = a + 10
-
-console.log(b);
+update()
